fe63ddc7b2e47a72939ef4ffee2016a55b3cde16
Suwijak Promsatid <suwijak.pak@gmail.com>
2023-09-04 21:31:50 +0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,25 @@
 const Header = () => {
     return(
         <>
-        <div class="navbar bg-base-100">
-        <div class="flex-1">
-            <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <div className="navbar bg-base-100">
+        <div className="flex-1">
+            <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
         </div>
-        <div class="flex-none gap-2">
-            <div class="form-control">
-            <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
+        <div className="flex-none gap-2">
+            <div className="form-control">
+            <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
             </div>
-            <div class="dropdown dropdown-end">
-            <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-                <div class="w-10 rounded-full">
+            <div className="dropdown dropdown-end">
+            <label tabindex="0" className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
                 <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
                 </div>
             </label>
-            <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+            <ul tabindex="0" className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
                 <li>
-                <a class="justify-between">
+                <a className="justify-between">
                     Profile
-                    <span class="badge">New</span>
+                    <span className="badge">New</span>
                 </a>
                 </li>
                 <li><a>Settings</a></li>
